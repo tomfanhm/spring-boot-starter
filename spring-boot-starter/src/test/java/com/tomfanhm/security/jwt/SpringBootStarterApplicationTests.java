dd99dc8610a83dab5a998599d8d1abb683c5d8b3
Tom Fan <119341778+TomFanHM@users.noreply.github.com>
2025-04-19 14:57:54 +0800

    feat: add initial Spring Boot application structure

---
+++ spring-boot-starter/src/test/java/com/tomfanhm/security/jwt/SpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomfanhm.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
